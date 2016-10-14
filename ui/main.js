7b2c5c408e5bf3ba32cea6c5eb84ffd5ad58015b
rohiniparethodiyil <rohuravip@gmail.com>
2016-10-14 21:32:48 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,12 @@ console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='NEW VALUE';
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    merginLeft=marginLeft+'px';
+}
 img.onclick=function(){
+    var interval=setInterval(moveRight,100);
     img.style.marginLeft='100px';
     
 };
